895625b2f77d01f6a65045dacbf4884e962c3da4
sheltowt <sheltowt@gmail.com>
2016-08-17 10:09:17 -0400

    adding updates

--- tree/app.js
+++ tree/app.js
@@ -5,7 +5,11 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/empower_csv', function(req, res){
-	res.sendFile(__dirname + 'empower.csv')
+	res.sendFile(__dirname + '/empower.csv')
+})
+
+app.get('/empower', function(req, res){
+	res.sendFile(__dirname + '/index.html')
 })
 
 
